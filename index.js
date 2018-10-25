bb74c95746b4736bed6f5d0994ae178749312f15
Evan Lawrence-Hurt <evanlh@gmail.com>
2018-10-24 23:29:35 -0400

    factor out NodeStyle, NodeShape and EdgeShape

--- index.js
+++ index.js
@@ -12,54 +12,90 @@ window.cluster = "digraph G {\n\n\tsubgraph cluster_0 {\n\t\tstyle=filled;\n\t\t
 // playing w/ mobx-state tree
 const { types, onSnapshot } = require('mobx-state-tree');
 
+
+const NodeStyle = types.model("NodeStyle", {
+  id: types.identifier,
+  bg: types.string,
+  fg: types.string,
+});
+
+const DEFAULTS = {
+  nodeStyle: NodeStyle.create({
+    id: "default",
+    bg: "#fff",
+    fg: "#000"
+  }),
+  nodeShape: "circle",
+  edgeShape: "normal",
+};
+
 const Node = types.model("Node", {
   id: types.identifier,
   label: types.string,
-  shape: types.enumeration("Shape", ["square", "circle"]),
-  bg: types.string,
-  fg: types.string,
+  style: types.reference(NodeStyle),
+  shape: types.enumeration("NodeShape", ["box", "polygon", "ellipse", "oval",
+                                     "circle", "point", "egg", "triangle",
+                                     "plaintext", "plain", "diamond", "trapezium",
+                                     "parallelogram", "house", "pentagon", "hexagon",
+                                     "septagon", "octagon", "doublecircle", "doubleoctagon",
+                                     "tripleoctagon", "invtriangle", "invtrapezium", "invhouse",
+                                     "Mdiamond", "Msquare", "Mcircle", "rect",
+                                     "rectangle", "square", "star", "none",
+                                     "underline", "cylinder", "note", "tab",
+                                     "folder", "box3d", "component", "promoter",
+                                     "cds", "terminator", "utr", "primersite",
+                                     "restrictionsite", "fivepoverhang",
+                                     "threepoverhang", "noverhang",
+                                     "assembly", "signature", "insulator", "ribosite",
+                                     "rnastab", "proteasesite", "proteinstab", "rpromoter",
+                                     "rarrow", "larrow", "lpromoter"
+                                    ]),
+
+  //edgesFrom: types.map(types.reference(Edge)),
+  //edgesTo: types.map(types.reference(Edge)),
 }).actions(self => ({
-  setForeground(fg) {
-    self.fg = fg;
-  },
-  setBackground(bg) {
-    self.bg = bg;
+  setStyle(style) {
+    this.style = style;
   }
 }));
 
 const Edge = types.model("Edge", {
+  id: types.identifier,
   source: types.reference(Node),
   target: types.reference(Node),
   label: types.string,
+  shape: types.enumeration("EdgeShape", ["box", "crow", "diamond", "dot", "inv", "none", "normal", "tee", "vee", "curve"]),
 });
 
 const Store = types.model("Store", {
-  nodes: types.array(Node),
-  edges: types.array(Edge),
+  nodes: types.map(Node),
+  edges: types.map(Edge),
 }).actions(self => ({
-  addNode (label, bg = "#fff", fg = "#000") {
-    self.nodes.push({ id: label, label, shape: "circle",  bg, fg });
+  addNode (id, label = id, style = DEFAULTS.nodeStyle, shape = DEFAULTS.nodeShape) {
+    console.debug("addNode", id, label, style);
+    self.nodes.put({ id, label, style, shape });
   },
-  addEdge (source, target, label) {
-    self.edges.push({ source, target, label });
+  addEdge (source, target, label, shape = DEFAULTS.edgeShape) {
+    console.debug("addEdge", id, source, target, label);
+    const id = source.id + "->" + target.id;
+    self.edges.put({ id, source, target, label, shape });
   },
 }));
 
 const store = Store.create({
-  nodes: [
-    { id: "A", label: "A", shape: "circle", bg: "#fff", fg: "#000" }
-  ],
-  edges: [
-  ]
+  nodes: {},
+  edges: {},
 });
 
 onSnapshot(store, snapshot => {
   console.dir(snapshot);
 });
 
+store.addNode("A");
 store.addNode("B");
-store.addEdge(store.nodes[0].id, store.nodes[1].id, "goes to");
-console.log(store.edges[0].source.label);
+store.addEdge(store.nodes.get("B"), store.nodes.get("A"), "goes to");
+store.addNode("B");
+
 
 
 
